b53007f9a692c35f2eb8a5c478f5f9babd006d22
Mathias Buus <mathiasbuus@gmail.com>
2018-06-22 09:09:27 +0200

    fast -> modulesIndexed, slow -> modules

--- 1-server-with-no-index.js
+++ 1-server-with-no-index.js
@@ -1,14 +1,15 @@
 const mongojs = require('mongojs')
 const fastify = require('fastify')
 
-const db = mongojs('localhost:27017/npm', ['fast', 'slow'])
+const db = mongojs('localhost:27017/npm')
+const col = db.collection('modules')
 const app = fastify()
 
 // get 5 newest and 5 oldest, no index
 app.get('/', function (req, reply) {
-  db.slow.find().sort({modified: -1}).limit(5, function (err, newest) {
+  col.find().sort({modified: -1}).limit(5, function (err, newest) {
     if (err) return reply.code(500).send(err)
-    db.slow.find().sort({modified: 1}).limit(5, function (err, oldest) {
+    col.find().sort({modified: 1}).limit(5, function (err, oldest) {
       if (err) return reply.code(500).send(err)
       reply.send({
         newest,
